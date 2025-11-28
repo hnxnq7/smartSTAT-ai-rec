8c35c02d2a621e26d71df52497ba0621a90a221c
Rachel Li <rachelliqx07@gmail.com>
2025-11-28 15:55:02 -0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repo name is not the root (e.g., username.github.io/repo-name)
+  // Uncomment and set the basePath:
+  // basePath: '/smartSTAT',
+  // assetPrefix: '/smartSTAT',
 }
 
 module.exports = nextConfig
